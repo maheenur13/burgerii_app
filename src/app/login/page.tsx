61c83f1423bd3356648b44eb7fec84e5ba9fb244
maheenur13 <differentmahee13@gmail.com>
2025-01-23 03:20:53 +0600

    refactor

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,8 +23,6 @@ export default function Login() {
             <Form
                 layout="vertical"
                 name="basic"
-                // labelCol={{ span: 8 }}
-                // wrapperCol={{ span: 16 }}
                 style={{ maxWidth: 600 }}
                 initialValues={{ remember: true }}
                 onFinish={onFinish}
